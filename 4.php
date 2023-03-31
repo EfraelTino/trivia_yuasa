e0b8a02615efbe902960bd4b13b03ac1578bc279
efrael <efrael2001@gmail.com>
2023-03-31 03:10:05 +0200

    s

--- 4.php
+++ 4.php
@@ -20,37 +20,91 @@ $dato = $_GET['dato'];
         padding: 0;
     }
 
+    html {
+        height: 100%;
+    }
+
     body {
+        /* overflow: hidden; */
+        margin: 0;
+        padding: 0;
+        min-height: 100%;
         box-sizing: border-box;
     }
 
-    .childs-4 {
-        background-image: url(img/fondo-33.jpg);
-        background-repeat: no-repeat;
+    .contenedor-4 {
+        background: url(img/fondo-33.jpg);
         background-size: cover;
-        background-position: center;
-        position: absolute;
-        top: 0%;
-        height: 100vh;
-        width: 100vw;
-        left: 0%;
-        /* transform: translate(-50%, -50%); */
-    }
-
-    .logo-pequenio {
-        position: absolute;
-        top: 4%;
-        left: 0;
+        position: fixed;
+        height: 100%;
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
     }
 
     .logo-recargate-4 {
-        width: 300px;
+        display: flex;
+        justify-content: space-between;
+        margin-top: 2rem;
+    }
 
+    .logo-pequenio {
+        width: 200px;
+        position: absolute;
+        left: 0;
     }
 
     .logo-fanalca-4 {
+        width: 100px;
         position: absolute;
-        top: 4%;
+        right: 0;
+    }
+
+    .logo-recargate-4 {
+        width: 200px;
+    }
+
+    .contenedor-recargate {
+        display: flex;
+        justify-content: center;
+    }
+
+    .my_row {
+        display: grid;
+
+    }
+
+    .border-vertical,
+    .border-vertical-2,
+    .border-vertical-3,
+    .border-vertical-4 {
+        display: grid;
+        align-content: center;
+        justify-items: center
+    }
+
+    .border-vertical,
+    .border-vertical-3 {
+        border-right: 2px solid #fff;
+    }
+
+    .border-vertical-3,
+    .border-vertical-4 {
+        border-top: 2px solid #fff;
+    }
+
+    .contenedor_preguntas {
+        position: relative;
+    }
+
+    .btn_puntajes {
+        background: none;
+        border: none;
+    }
+
+    .btn_puntajes:focus {
+        border: none;
     }
 
     .contenedor_preguntas {
@@ -63,12 +117,30 @@ $dato = $_GET['dato'];
 
     .numero-encima {
         position: absolute;
-        top: 1%;
-        left: 6%;
+        top: 6%;
+        left: 5%;
         font-size: 1.3rem;
         color: white;
         font-weight: 900;
         font-style: italic;
+    }
+
+    .img-22 {
+        width: 300px;
+    }
+
+    .btn_puntajes .numero_respuesta {
+        color: white;
+        font-family: var(--font-sans);
+        font-weight: 700;
+        font-size: .8rem;
+        font-style: italic;
+        transition: transform 0.3s ease;
+        margin: 0 .3rem;
+    }
+
+    .btn_puntajes .numero_respuesta:hover {
+        transform: scale(1.2);
     }
 
     .pregunta-encima {
@@ -77,52 +149,21 @@ $dato = $_GET['dato'];
         left: 8%;
         font-size: .7rem;
         color: white;
-        font-weight: 900;
+        font-weight: 800;
         font-style: italic;
         padding-right: 3rem !important;
         -webkit-text--width: 0px;
         text-shadow: -2px 0px 5px rgba(250, 255, 94, 0.75);
+
     }
 
     .tiempo-encima {
         position: absolute;
-        top: 25%;
-        left: 88%;
+        top: 28%;
+        left: 89%;
         font-size: .8rem;
         color: white;
         font-weight: 900;
-
-    }
-
-    .border-vertical {
-        border-right: 2px solid white;
-    }
-
-    .border-vertical-3 {
-        border-right: 2px solid white;
-        border-top: 2px solid white;
-    }
-
-    .border-vertical-4 {
-        border-top: 2px solid white;
-    }
-
-    .btn_puntajes {
-        background: none;
-        border: none;
-        transition: transform 0.2s ease-in-out;
-    }
-
-    .btn_puntajes:hover {
-        transform: scale(1.05);
-    }
-
-    .btn_puntajes .numero_respuesta {
-        color: white;
-        font-family: var(--font-sans);
-        font-weight: 700;
-        font-size: 1rem;
-        font-style: italic;
 
     }
 
@@ -168,222 +209,250 @@ $dato = $_GET['dato'];
         font-weight: 900;
     }
 
+    .swal2-cancel:focus,
+    .swal2-confirm:active {
+        outline: none !important;
+    }
+
     .swal2-html-container {
         color: white;
     }
 
-    @media (min-width:384px) {
-        .border-vertical-2 {
-            border-right: 2px solid white;
-        }
+    :focus {
+        outline: none;
+    }
 
-        .border-vertical-3 {
-            border-top: none;
-        }
-
-        .border-vertical-4 {
-            border-top: none;
+    @media (max-width:575px) {
+        .my_row {
+            grid-template-columns: repeat(2, 1fr);
+            grid-template-rows: repeat(2, 1fr);
         }
     }
 
     @media (min-width:576px) {
+        .my_row {
+            grid-template-columns: repeat(4, 1fr);
+            grid-template-rows: 1fr;
+        }
+
+        .numero-encima {
+            top: 20%;
+            left: 6%;
+        }
+
         .img-22 {
             width: 530px;
         }
 
-        .numero-encima {
-            top: 18%;
-            left: 5%;
+        .logo-pequenio {
+            width: 300px;
+        }
+
+        .logo-fanalca-4 {
+            width: 150px;
+        }
+
+        .border-vertical-2 {
+            border-right: 2px solid #fff;
+        }
+
+        .border-vertical-3,
+        .border-vertical-4 {
+            border-top: none;
         }
 
         .pregunta-encima {
             top: 52%;
             left: 8%;
             font-size: 1.2rem;
-            padding-right: 5.3rem !important;
+            padding-right: 5rem !important;
         }
 
         .tiempo-encima {
-            top: 26%;
+            top: 25%;
             left: 88%;
-            font-size: 1.3rem;
-        }
-
-        .border-vertical {
-            border-right: 2px solid white;
-
-        }
-
-        .border-vertical-3 {
-            border-right: 2px solid white;
-            border-top: none;
-        }
-
-        .border-vertical-4 {
-            border-top: none;
+            font-size: 1.5rem;
         }
 
 
     }
 
-    @media(min-device-width:768px) {
-        .childs-4 {
-            height: 100vh;
-        }
+    @media (min-width:768px) {
 
-        .logo-recargate-4 {
-            width: 440px;
+        .numero-encima {
+            top: 16%;
+            left: 6%;
+            font-size: 2rem;
         }
 
         .img-22 {
             width: 700px;
         }
 
-        .numero-encima {
-            top: 17%;
-            left: 5%;
-            font-size: 2rem;
-        }
-
-        .btn_puntajes .numero_respuesta {
-            font-size: 1.3rem;
+        .logo-recargate-4 {
+            width: 420px;
         }
 
         .pregunta-encima {
             top: 55%;
             left: 8%;
             font-size: 1.5rem;
-            padding-right: 5.7rem !important;
+            padding-right: 5rem !important;
+        }
+
+        .logo-pequenio {
+            width: 400px;
+        }
+
+        .logo-fanalca-4 {
+            width: 200px;
         }
 
         .tiempo-encima {
-            top: 27%;
+            top: 26%;
             left: 88%;
-            font-size: 1.8rem;
+            font-size: 2rem;
         }
 
-        .border-vertical-2 {
-            border-right: 2px solid white;
-        }
-    }
-
-    @media (min-width:920px) {
-        .childs-4 {
-            height: 100vh;
+        .btn_puntajes .numero_respuesta {
+            font-size: 1rem;
         }
     }
 
     @media (min-width:1200px) {
-        .childs-4 {
-            height: 100vh;
+        .contenedor-4 {
+            height: 100%;
+        }
+
+        .numero-encima {
+            top: 18%;
+            left: 6%;
+            font-size: 2.5rem;
+        }
+
+        .pregunta-encima {
+            top: 65%;
+            left: 8%;
+            font-size: 1.8rem;
+            padding-right: 4rem !important;
+        }
+
+        .logo-pequenio {
+            width: 400px;
+        }
+
+        .logo-fanalca-4 {
+            right: 5%;
         }
 
         .img-22 {
             width: 920px;
         }
 
-        .numero-encima {
-            top: 18%;
-            left: 5%;
-            font-size: 2.5rem;
+        .logo-recargate-4 {
+            width: 500px;
         }
 
-        .pregunta-encima {
+        .logo-pequenio {
             top: 10%;
-            left: 11%;
-            font-size: 1.8rem;
-            padding-right: 6.5rem !important;
+        }
+
+        .logo-fanalca-4 {
+            top: 9%;
         }
 
         .tiempo-encima {
             top: 26%;
-            left: 88%;
-            font-size: 2.3rem;
+            font-size: 2.5rem;
         }
 
+        .btn_puntajes .numero_respuesta {
+            font-size: 1.3rem;
+        }
     }
 
     @media (min-width:1500px) {
-        .childs-4 {
-            height: 100vh;
-        }
-
-        .img-22 {
-            margin-top: -3rem;
-            width: 1200px;
-        }
-
         .numero-encima {
-            top: 3%;
-            left: 5%;
+            top: 20%;
+            left: 6%;
             font-size: 3rem;
         }
 
         .pregunta-encima {
-            top: 55%;
+            top: 65%;
             left: 8%;
             font-size: 2.3rem;
-            padding-right: 7.5rem !important;
+            padding-right: 5rem !important;
+        }
+
+        .img-22 {
+            width: 1200px;
         }
 
         .tiempo-encima {
-            top: 13%;
-            left: 89%;
-            font-size: 2.5rem;
+            top: 24%;
+            left: 87.8%;
+            font-size: 3.5em;
+        }
+
+        .btn_puntajes .numero_respuesta {
+            font-size: 1.5rem;
         }
     }
 </style>
 
 <body>
-    <div class="childs-4 py-5">
-        <div class="container">
-            <div class="row">
-                <div class="col-12 col-lg-6 d-none d-lg-flex  "><img src="img/yuasa-novelbat-logo-pequenio.png" alt="" width="620px" class="logo-pequenio"></div>
-                <div class="col-12 col-lg-6 d-flex justify-content-end align-items-center"><img src="img/FANALCA_LOGO.png" alt="" width="220px" class="logo-fanalca-4"></div>
-                <div class="col-12 d-flex justify-content-center">
+    <div class="contenedor-4 py-5">
+        <div class="contendor_general">
+            <div class="">
+                <div class=""><img src="img/yuasa-novelbat-logo-pequenio.png" alt="" class="logo-pequenio"></div>
+                <div class=""><img src="img/FANALCA_LOGO.png" alt="" width="220px" class="logo-fanalca-4"></div>
+            </div>
+            <div class="contenedor-recargate">
+                <div class="d-flex justify-content-center">
                     <img src="img/logo-recargate.png" alt="" class="logo-recargate-4">
                 </div>
-                <div class="col-12 d-flex justify-content-center m-0 p-0">
-                    <div class="contenedor_preguntas m-0 p-0" id="contenedor_preguntas">
-                        <img src="img/22.png" alt="" class="img-22">
-                        <h4 class="numero-encima m-0 p-0" id="numero">1</h4>
-                        <div class="contenendor-pregunta" id="contenedor-pregunta">
-                            <h4 class="pregunta-encima text-center p-0 m-0" id="pregunta">
-                                ¿En qué año se crea Yuasa Battery Manufacturing?</h4>
-                        </div>
-                        <h3 class="tiempo-encima m-0 p-0" id="reloj">20</h3>
+            </div>
+            <div class="col-12 d-flex justify-content-center m-0 p-0">
+                <div class="contenedor_preguntas m-0 p-0" id="contenedor_preguntas">
+                    <img src="img/22.png" alt="" class="img-22">
+                    <h4 class="numero-encima m-0 p-0" id="numero">1</h4>
+                    <div class="contenendor-pregunta" id="contenedor-pregunta">
+                        <h4 class="pregunta-encima text-center p-0 m-0" id="pregunta">
+                            ¿En qué año se crea Yuasa Battery Manufacturing?</h4>
                     </div>
+                    <h3 class="tiempo-encima m-0 p-0" id="reloj">20</h3>
                 </div>
-                <div class="col-12 col-sm-12 container align-items-center align-self-center container">
-                    <div class="row ">
-                        <div class="col-6 col-md-3 d-sm-flex flex-sm-column justify-content-center align-items-center  m-0 p-0 border-vertical pb-1">
-                            <img src="img/27.png" alt="" width="100px">
-                            <button class="btn_puntajes m-0 p-0 ">
-                                <br>
-                                <span class="numero_respuesta d-flex justify-content-center px-3 py-2" id="btn1" onclick="oprimir_btn(0)" style="border-radius: 30px;">0</span>
-                            </button>
-                        </div>
-                        <div class="col-6 col-md-3 d-sm-flex m-0 p-0  flex-sm-column justify-content-center align-items-center border-vertical-2 pb-1">
-                            <img src="img/28.png" alt="" width="100px">
-                            <button class="btn_puntajes m-0 p-0 ">
-                                <br>
-                                <span class="numero_respuesta d-flex justify-content-center px-3 py-2" id="btn2" onclick="oprimir_btn(1)" style="border-radius: 30px;">0</span>
-                            </button>
-                        </div>
-                        <div class="col-6 col-md-3 d-sm-flex m-0 p-0  flex-sm-column justify-content-center align-items-center   border-vertical-3 pb-1">
-                            <img src="img/29.png" alt="" width="100px">
-                            <button class="btn_puntajes m-0 p-0 ">
-                                <br>
-                                <span class="numero_respuesta d-flex justify-content-center px-3 py-2" id="btn3" onclick="oprimir_btn(2)" style="border-radius: 30px;">0</span>
-                            </button>
-                        </div>
-                        <div class="col-6 col-md-3 d-sm-flex m-0 p-0 align-items-center justify-content-center flex-sm-column border-vertical-4 pb-1">
-                            <img src="img/30.png" alt="" width="100px">
-                            <button class="btn_puntajes m-0 p-0 ">
-                                <br>
-                                <span class="numero_respuesta d-flex justify-content-center px-3 py-2" id="btn4" onclick="oprimir_btn(3)" style="border-radius: 30px;">0</span>
-                            </button>
-                        </div>
+            </div>
+            <div class="col-12 col-sm-12 container align-items-center align-self-center container">
+                <div class="row ">
+                    <div class="col-6 col-md-3 d-sm-flex flex-sm-column justify-content-center align-items-center  m-0 p-0 border-vertical pb-1">
+                        <img src="img/27.png" alt="" width="100px">
+                        <button class="btn_puntajes m-0 p-0 ">
+                            <br>
+                            <span class="numero_respuesta d-flex justify-content-center px-3 py-2" id="btn1" onclick="oprimir_btn(0)" style="border-radius: 30px;">0</span>
+                        </button>
+                    </div>
+                    <div class="col-6 col-md-3 d-sm-flex m-0 p-0  flex-sm-column justify-content-center align-items-center border-vertical-2 pb-1">
+                        <img src="img/28.png" alt="" width="100px">
+                        <button class="btn_puntajes m-0 p-0 ">
+                            <br>
+                            <span class="numero_respuesta d-flex justify-content-center px-3 py-2" id="btn2" onclick="oprimir_btn(1)" style="border-radius: 30px;">0</span>
+                        </button>
+                    </div>
+                    <div class="col-6 col-md-3 d-sm-flex m-0 p-0  flex-sm-column justify-content-center align-items-center   border-vertical-3 pb-1">
+                        <img src="img/29.png" alt="" width="100px">
+                        <button class="btn_puntajes m-0 p-0 ">
+                            <br>
+                            <span class="numero_respuesta d-flex justify-content-center px-3 py-2" id="btn3" onclick="oprimir_btn(2)" style="border-radius: 30px;">0</span>
+                        </button>
+                    </div>
+                    <div class="col-6 col-md-3 d-sm-flex m-0 p-0 align-items-center justify-content-center flex-sm-column border-vertical-4 pb-1">
+                        <img src="img/30.png" alt="" width="100px">
+                        <button class="btn_puntajes m-0 p-0 ">
+                            <br>
+                            <span class="numero_respuesta d-flex justify-content-center px-3 py-2" id="btn4" onclick="oprimir_btn(3)" style="border-radius: 30px;">0</span>
+                        </button>
                     </div>
                 </div>
             </div>
@@ -416,7 +485,7 @@ $dato = $_GET['dato'];
                     if (pregunt.length < 30) {
                         ePregunt.style.top = "55" + "%";
                         ePregunt.style.left = "25" + "%";
-                    }else if (pregunt.length < 60) {
+                    } else if (pregunt.length < 60) {
                         ePregunt.style.top = "55" + "%";
                         ePregunt.style.left = "13" + "%";
                     }
